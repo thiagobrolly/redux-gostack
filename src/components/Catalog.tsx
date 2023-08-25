8fa449cfdeeb2464d0ee544a4b03070c7b35bb61
Thiago Brolly <thiagoex377@gmail.com>
2023-08-25 17:35:36 -0300

    Iniciando Redux

---
+++ src/components/Catalog.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { IProduct } from '../store/modules/cart/types';
+import { api } from '../services/api';
+import { CatalogItem } from './CatalogItem';
+
+export function Catalog() {
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
+  // const catalog = useSelector(state => state);
+
+  // console.log(catalog);
+
+  useEffect(() => {
+    api.get('products').then((res) => {
+      setCatalog(res.data);
+    });
+  }, []);
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+      {catalog.map((product) => (
+        <CatalogItem key={product.id} product={product}/>
+      ))}
+    </main>
+  );
+}
